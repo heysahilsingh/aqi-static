e65ebf3c23cb24b670cb7254b0bc0bdc7fbe8196
Sahil <sahilsworkmail@gmail.com>
2023-12-01 17:53:28 +0530

    Static data completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,27 @@
-console.log("first")
+// Off canvas mobile menu
+document.querySelector("header .mob-menu-toggler").addEventListener("click", () => {
+    document.querySelector("header .mob").classList.toggle("off-canvas-active")
+})
+
+// UI TOGGLE ELEMENT
+const uiToggles = document.querySelectorAll(".ui-toggle");
+
+uiToggles.forEach(selector => {
+    const toggleHandler = selector.querySelector(".ui-toggle-handler");
+
+    toggleHandler.addEventListener("click", () => {
+        selector.classList.toggle("active");
+    });
+});
+
+document.addEventListener("click", (event) => {
+    uiToggles.forEach(selector => {
+        if (selector.classList.contains("ui-toggle-auto-close") && !selector.contains(event.target)) {
+            selector.classList.remove("active");
+        }
+    });
+});
+
 
 // Update Footer Credit year 
 document.querySelector("#footer-credit span").innerText = new Date().getFullYear();
